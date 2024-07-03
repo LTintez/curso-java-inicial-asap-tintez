0862a1079f5a652517ecc9204a0208e0e6e623fe
Lautaro Tintez <ltintez@asap-consulting.net>
2024-07-03 10:43:48 -0300

    resolucion ejercicio 1

--- src/main/java/Clase1/Ejercicio1.java
+++ src/main/java/Clase1/Ejercicio1.java
@@ -1,9 +1,33 @@
 package Clase1;
 
+import java.util.Scanner;
+
+/**
+ * Programa que lee 1 numero entero por teclado e indica si es multiplo de 2 o de 3.
+ *
+ * @author LTintez
+ *
+ */
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa un numero entero: ");
+
+        int num = scanner.nextInt();
+
+        if (num % 2 == 0 && num % 3 == 0) {
+            System.out.println("El numero ingresado es multiplo de 2 y 3");
+        } else if (num % 2 == 0) {
+            System.out.println("El numero ingresado es multiplo de 2");
+        } else if (num % 3 == 0) {
+            System.out.println("El numero ingresado es multiplo de 3");
+        } else {
+            System.out.println("El numero ingresado no es multiplo de 2 ni de 3");
+        }
+
     }
 
 
